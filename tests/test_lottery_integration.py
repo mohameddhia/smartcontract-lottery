eabec1f0db182cee21e1f127affede04c374866e
dhialinux <dhialinux@pop-os.localdomain>
2021-12-21 12:43:53 +0100

    commit smartcontract-lottery

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,26 @@
+from brownie import network
+from brownie.network import account
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+)
+import pytest
+from scripts.deploy_lottery import deploy_lottery
+import time
+
+
+def test_can_pick_winner():
+    # Arrange
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(160)
+    assert lottery.recentWinner() == account.address
+    assert lottery.balance() == 0
